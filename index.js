d5591807a29027704777f449311e36fea19d6f25
Melena10 <eakudinova1980@gmail.com>
2022-08-08 21:53:33 +0300

    наполнение файла js
    1. Создали переменные для элементов из HTML
    2. Обработали клик по кнопке “Начать”
    3. По клику на кнопку поменяли значение заголовка h1, дату отсчета пока только запомнили  в переменную
    4. Проверили  значения даты, была ли выбрана любая дата (не пустое ли значение)
    5. Скрыли кнопку “Начать”, отобразили кнопку “Сбросить”. Скрыли блок с инпутами, отобразили блок с классом `output`

--- index.js
+++ index.js
@@ -1,2 +1,35 @@
-'use strict';
+"use strict";
 
+const title = document.querySelector("#title-date");
+const date = document.querySelector("#date");
+
+const buttonStart = document.querySelector("#btn");
+const buttonReset = document.querySelector("#btn-reset");
+
+const numbers = document.querySelector(".numbers");
+const timeLabels = document.querySelector(".time-labels");
+const h1 = document.querySelector("h1");
+
+const input = document.querySelector(".input");
+const output = document.querySelector(".output");
+
+buttonStart.addEventListener("click", countDown);
+
+function countDown() {
+  let newData = date.value;
+  if (newData !== "") {
+    let newTitle = h1.textContent = title.value;
+
+    console.log(newData);
+    console.log(newTitle);
+
+    input.classList.add("hide");
+    output.classList.remove("hide");
+
+    buttonStart.add("hide");
+    buttonReset.remove("hide");
+
+  } else {
+    alert("Пожалуйста, введите дату");
+  }
+}
